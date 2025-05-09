fc1a21debd98f1e7c97af818dfbeacc319d43cd4
Fabián Leyva <fleyva.home@outlook.com>
2025-05-09 14:31:39 -0600

    initial commit

---
+++ src/main/java/com/eyvot/kctapp/KctappApplication.java
@@ -0,0 +1,13 @@
+package com.eyvot.kctapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KctappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KctappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyvot/kctapp/KctappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyvot.kctapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KctappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
