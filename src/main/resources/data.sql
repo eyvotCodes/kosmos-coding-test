c753acf1f64984351650d0cbff5f6a44fbe755d0
Fabián Leyva <fleyva.home@outlook.com>
2025-05-09 16:07:31 -0600

    add 8 citas for the same day and doctor in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,4 +14,12 @@ INSERT INTO consultorio (numero, piso) VALUES
 
 -- Citas
 INSERT INTO cita (horario_inicio, horario_fin, nombre_paciente, doctor_id, consultorio_id) VALUES
-('2025-05-09 10:30:00', '2025-05-09 11:15:00', 'Ramón Emiliano Cázares López', 4, 1);
+('2025-05-09 10:30:00', '2025-05-09 11:15:00', 'Ramón Emiliano Cázares López', 4, 1),
+('2025-05-09 09:00:00', '2025-05-09 10:00:00', 'Carlos Méndez Escobar', 5, 3),
+('2025-05-09 10:00:00', '2025-05-09 11:00:00', 'María Fernanda López Silva', 5, 2),
+('2025-05-09 11:00:00', '2025-05-09 12:00:00', 'Luis Rodríguez Paredes', 5, 3),
+('2025-05-09 12:00:00', '2025-05-09 13:00:00', 'Fernanda Cruz Morales', 5, 1),
+('2025-05-09 13:00:00', '2025-05-09 14:00:00', 'Jorge Andrés Salazar Díaz', 5, 2),
+('2025-05-09 15:00:00', '2025-05-09 16:00:00', 'Valeria Torres Ramírez', 5, 3),
+('2025-05-09 16:00:00', '2025-05-09 17:00:00', 'Raúl Eduardo Martínez Soto', 5, 1),
+('2025-05-09 17:00:00', '2025-05-09 18:00:00', 'Sofía Aguilar Herrera', 5, 2);
